6ab9728e2f053b476f00b6c8785e3e28dd4eb328
VolkovD1Icip-19 <72722558+VolkovD1Icip-19@users.noreply.github.com>
2022-02-18 13:14:21 +0500

    Add files via upload

---
+++ Director/Pages/Expenses.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp37.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Expenses.xaml
+    /// </summary>
+    public partial class Expenses : Page
+    {
+        DepoEntities2 db = new DepoEntities2();
+        public Expenses()
+        {
+            InitializeComponent();
+            dg1.ItemsSource = db.Expenses.ToList();
+        }
+    }
+}
